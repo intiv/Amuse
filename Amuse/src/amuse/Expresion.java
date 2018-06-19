d828280a168a616b7c05f2d150e3088f2d928500
Inti Velasquez <intiv@unitec.edu>
2018-06-19 14:46:13 -0600

    Metodos fusiona, completa iniciales. Reestructuracion de COND, inicio de cuadruplos en COND

---
+++ Amuse/src/amuse/Expresion.java
@@ -0,0 +1,15 @@
+package amuse;
+
+import java.util.ArrayList;
+
+public class Expresion{
+    public ArrayList<Integer> listav = new ArrayList();
+    public ArrayList<Integer> listaf = new ArrayList();
+
+    public Expresion(){
+        listav = new ArrayList();
+        listaf = new ArrayList();
+    }
+
+    
+}
