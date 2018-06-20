c231df650915ef8db5a30222c4ee5fd7c274395a
Inti Velasquez <intiv@unitec.edu>
2018-06-20 15:11:44 -0600

    Arreglado COND y completa listav

--- Amuse/src/amuse/Expresion.java
+++ Amuse/src/amuse/Expresion.java
@@ -5,11 +5,32 @@ import java.util.ArrayList;
 public class Expresion{
     public ArrayList<Integer> listav = new ArrayList();
     public ArrayList<Integer> listaf = new ArrayList();
+    public int cuad;
+    public boolean or = false;
+
+    public Expresion(boolean or){
+        listav = new ArrayList();
+        listaf = new ArrayList();
+        this.or = or;
+    }
 
     public Expresion(){
         listav = new ArrayList();
         listaf = new ArrayList();
     }
-
+    
+    @Override
+    public String toString(){
+        String retVal = "Listav:\n";
+        for(int i = 0; i < listav.size(); i++){
+            retVal+=listav.get(i).intValue()+" ";
+        }
+        retVal += "\nListaf:\n";
+        for(int i = 0; i < listaf.size(); i++){
+            retVal+=listaf.get(i).intValue()+" ";
+        }
+        retVal+="\n";
+        return retVal;
+    }
     
 }
