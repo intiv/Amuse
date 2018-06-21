654437b4cb0ee122821b6d8f73f0cbdde3f0d3d8
juany-ramirez <juany.ramrz@unitec.edu>
2018-06-20 22:45:54 -0600

    Arbol fallido

--- Amuse/src/amuse/AmuseMain.java
+++ Amuse/src/amuse/AmuseMain.java
@@ -24,88 +24,90 @@ public class AmuseMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try{
+        try {
 //            buildParser();
             File file = new File("./src/amuse/Ejemplos/ejemplo1.txt");
             FileReader fr = new FileReader(file);
             scanner lex = new scanner(fr);
             Sintactico sint = new Sintactico(lex);
             Symbol simbolo = sint.parse();
-            if(sint.hayErrores==0){
-                System.out.println("No hubieron errores");
-                Nodo raiz = sint.padre;
-                Graficar(recorrido1(raiz),"Amuse_Tree");
+            System.out.println("No hubieron errores");
+            Nodo raiz = sint.padre;
+            Graficar(recorrido1(raiz), "Amuse_Tree");
+            if (sint.hayErrores == 0) {
+                
             }
             System.out.println(simbolo);
         } catch (Exception ex) {
             Logger.getLogger(AmuseMain.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public static String recorrido1(Nodo raiz){
+
+    public static String recorrido1(Nodo raiz) {
         String cuerpo = "";
-        cuerpo += "nodo"+raiz.idNod +"[label=\""+cambiar(raiz.valor)+"("+raiz.Etiqueta+")\"];\n";
-       for(Nodo hijos:raiz.hijos){
-           if(hijos.Etiqueta!=null){
-               cuerpo += "nodo"+raiz.idNod +"[label=\""+cambiar(raiz.valor)+"("+raiz.Etiqueta+")\"];\n";
-               cuerpo += "nodo"+ hijos.idNod +"[label=\""+cambiar(hijos.valor)+"("+hijos.Etiqueta+")\"];\n";
-               cuerpo += raiz.idNod + "->" +hijos.idNod+"\n";
-               cuerpo +=  recorrido1(hijos);    
-           }           
-      }
+        cuerpo += "nodo" + raiz.idNod + "[label=\"" + cambiar(raiz.valor) + "(" + raiz.Etiqueta + ")\"];\n";
+        System.out.println(cuerpo);
+        for (Nodo hijos : raiz.hijos) {
+            if (hijos.Etiqueta != null) {
+                cuerpo += "nodo" + raiz.idNod + "[label=\"" + cambiar(raiz.valor) + "(" + raiz.Etiqueta + ")\"];\n";
+                cuerpo += "nodo" + hijos.idNod + "[label=\"" + cambiar(hijos.valor) + "(" + hijos.Etiqueta + ")\"];\n";
+                cuerpo += raiz.idNod + "->" + hijos.idNod + "\n";
+                cuerpo += recorrido1(hijos);
+            }
+        }
+        System.out.println(cuerpo);
         return cuerpo;
     }
-    
-    public static String cambiar(String texto){
-        if(texto != null){
-            texto = texto.replaceAll("\"","");
-            
-        }else{
-            texto ="";
+
+    public static String cambiar(String texto) {
+        if (texto != null) {
+            texto = texto.replaceAll("\"", "");
+
+        } else {
+            texto = "";
         }
         return texto;
     }
-    
-    public static void Graficar(String cadena, String nombre){
+
+    public static void Graficar(String cadena, String nombre) {
         FileWriter fichero = null;
         PrintWriter pw = null;
-        String archivo = nombre+".dot";
-        try{
+        String archivo = nombre + ".dot";
+        try {
             fichero = new FileWriter(archivo);
             pw = new PrintWriter(fichero);
             pw.println("digraph G {node[shape=box, style=filled, color=Gray95]; edge[color=black];rankdir=TB \n");
             pw.println(cadena);
             pw.println("\n }");
             fichero.close();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
-        try{
+        try {
             //String cmd = "dot -Tpng Amuse_Tree.dot -o Amuse_Tree.png";
-            String cmd = "dot -Tpng "+ nombre + ".dot -o " + nombre + ".png";
+            String cmd = "dot -Tpng " + nombre + ".dot -o " + nombre + ".png";
             Runtime.getRuntime().exec(cmd);
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e);
         }
     }
-    
-    
-    synchronized private static void buildParser() {
-       String params[] = new String[7];
 
-       params[0] = "-destdir";
-       params[1] = "src/amuse/";
-       params[2] = "-parser";
-       params[3] = "Sintactico";
-       params[4] = "-symbols";
-       params[5] = "Amuse";
-       
-       params[6] = "src/amuse/AmuseSyntactic.cup";
-       try {
-           java_cup.Main.main(params);
-       } catch (Exception ex) {
-           Logger.getLogger(Amuse.class.getName()).log(Level.SEVERE, null, ex);
-       }
-   }
-    
+    synchronized private static void buildParser() {
+        String params[] = new String[7];
+
+        params[0] = "-destdir";
+        params[1] = "src/amuse/";
+        params[2] = "-parser";
+        params[3] = "Sintactico";
+        params[4] = "-symbols";
+        params[5] = "Amuse";
+
+        params[6] = "src/amuse/AmuseSyntactic.cup";
+        try {
+            java_cup.Main.main(params);
+        } catch (Exception ex) {
+            Logger.getLogger(Amuse.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
